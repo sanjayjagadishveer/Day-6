7d08dceb0b8d6ddf9d4fb8382e44ea788a6dc110
sanjayjagadishveer <sanjayjagadish8@gmail.com>
2022-03-28 23:25:05 +0530

    add my files

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+const showname=(firstname,lastname, ...title)=>
+{
+    let name=firstname+" "+lastname+ " ";
+    for(let data of title)
+    {
+        name+=data+" ";
+    }
+    return name;
+}
+console.log(showname('sanjay','jagadish','veer','from','bangalore'));*/
+
+/*let arr1=[1,2,3];
+let arr2=[3,4,5];
+
+let merge=[...arr1,...arr2];
+console.log(arr1);
+console.log(arr2);
+console.log(merge);*/
+
+/*function merge(array1,array2)
+{
+    return [...array1,...array2];
+}
+let arr1=[1,2,3];
+let arr2=[3,4,5];
+
+console.log(merge(arr1,arr2));*/
